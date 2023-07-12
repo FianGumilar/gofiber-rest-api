7f09e1447f82e1a9fd553711fde11311cd652e17
FianGumilar <fiangumilar@gmail.com>
2023-07-12 10:56:26 +0700

    generate JWT & Verify token

---
+++ controllers/login.controller.go
@@ -0,0 +1,76 @@
+package controllers
+
+import (
+	"time"
+
+	"github.com/FianGumilar/gofiber-rest-api/database"
+	"github.com/FianGumilar/gofiber-rest-api/models"
+	"github.com/FianGumilar/gofiber-rest-api/utils"
+	"github.com/go-playground/validator/v10"
+	"github.com/gofiber/fiber/v2"
+	"github.com/golang-jwt/jwt"
+)
+
+func Login(c *fiber.Ctx) error {
+	loginRequest := new(models.Login)
+
+	if err := c.BodyParser(loginRequest); err != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"Message": err.Error(),
+		})
+	}
+
+	// Validation
+	validate := validator.New()
+	errValidate := validate.Struct(loginRequest)
+	if errValidate != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"message": "Failed to Login",
+			"error":   errValidate.Error(),
+		})
+	}
+
+	var user models.User
+
+	// Check Validation Email
+	errEmail := database.DB.Debug().First(&user, "email = ?", loginRequest.Email).Error
+	if errEmail != nil {
+		return c.Status(400).JSON(fiber.Map{
+			"message": "User not found",
+		})
+	}
+
+	// Check Password Validation
+	isValid := utils.CheckHash(loginRequest.Password, user.Password)
+	if !isValid {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"message": "Password is not valid",
+		})
+	}
+
+	// Generate JWT
+	claims := jwt.MapClaims{}
+	// Claims or Payloads Definitions
+	claims["name"] = user.Name
+	claims["email"] = user.Email
+	claims["phone"] = user.Phone
+	claims["exp"] = time.Now().Add(2 * time.Minute).Unix()
+
+	if user.Email == "fafa@gmail.com" {
+		claims["role"] = "admin"
+	} else {
+		claims["role"] = "user"
+	}
+
+	token, errGenerate := utils.GenerateToken(&claims)
+
+	if errGenerate != nil {
+		return c.Status(401).JSON(fiber.Map{
+			"error": "Token is not valid",
+		})
+	}
+
+	return c.Status(fiber.StatusOK).JSON(fiber.Map{
+		"token": token,
+	})
+}
--- middleware/middleware.go
+++ middleware/middleware.go
@@ -1,13 +1,33 @@
 package middleware
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"github.com/FianGumilar/gofiber-rest-api/utils"
+	"github.com/gofiber/fiber/v2"
+)
 
 func UserAuth(c *fiber.Ctx) error {
-	token := c.Get("token")
-	if token != "secret" {
+	token := c.Get("token") // Get token header
+	if token == "" {
 		return c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{
+			"message": "Token is required",
+		})
+	}
+
+	//_, err := utils.VerifyToken(token)
+	claims, err := utils.DecodeToken(token)
+
+	if err != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
 			"message": "Unauthorized",
 		})
 	}
+
+	role := claims["role"].(string)
+	if role != "admin" {
+		return c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{
+			"message": "You are not admin",
+		})
+	} 
+
 	return c.Next()
 }
